37a87fae7d3fe2fa16c242383088e74cbc67b12c
Yair Gonzalez <dyg09032003@gmail.com>
2024-08-12 22:36:16 -0500

    'init'

---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,16 @@
+import { CartCounter } from "@/app/shoping-cart";
+
+export const metadata = {
+  title: "shopping  cart",
+  description: "Un simple contador",
+};
+
+export default function page() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+
+      <CartCounter value={5} />
+    </div>
+  );
+}
